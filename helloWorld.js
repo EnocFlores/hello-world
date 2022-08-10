3374f8e305b579b987c534022f5d0d4b2d67c4de
EnocFlores <enoc.flores.ef@gmail.com>
2022-08-10 16:14:20 -0400

    Adds javascript file helloWorld.js

---
+++ helloWorld.js
@@ -0,0 +1,87 @@
+// Comments in javascript
+
+
+
+
+
+
+
+
+
+
+
+/*** OBJECTS ***/
+let anObject = {
+	property1 = "aValue",
+	property2 = "aNumValue",
+	property3 = 7
+};
+
+let callDotObject = anObject.property1;
+let callBracketObject = anObject['property2'];
+
+
+
+/*** OBJECT CONSTRUCTOR ***/
+function PrototypeObject(property1, property2, property3) {
+	this.property1 = property1;
+	this.property2 = property2;
+	this.property3 = property3;
+};
+
+
+
+/*** ARRAYS ***/
+let anArray = [ "thing1", "thing2", "thing3", "etc..." ];
+anArray[3]; //etc...
+
+anArray.pop();
+// anArray = [ "thing1", "thing2", "thing3" ];
+
+anArray.push('thing4');
+// anArray = [ "thing1", "thing2", "thing3", "thing4" ];
+
+// methods for queues and stacks
+// .pop() .push("thingToAdd")
+// .shift() .unshift("thingToAdd")
+
+// apppend arrays
+// array1.concat(array2)
+
+// cut up array, return a range
+// anArray.slice(0, -2) // return whole array except last two
+
+
+
+/*** FUNCTIONS ***/
+function nameOfFunction () {
+	return something="returning value from function"
+};
+
+
+
+/*** BEYOND BOOLEANS ***/
+// Falsy Values
+// (false);
+// (0);
+// (");
+// (null);
+// (undefined);
+// (NaN);
+
+
+
+/*** REGEX ***/
+// /pattern/modifiers;
+// Modifiers:
+// 	i (case insensitive)
+// 	g (global)
+
+// How to find matches? use .match(regex) method
+// How to search for index? use .search(regex) method
+
+let regEx = /ou/i;
+
+
+
+/*** ***/
